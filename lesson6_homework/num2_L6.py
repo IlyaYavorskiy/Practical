42ac0486ac2554394d555185e4d7e80b9437c018
IlyaYavorskiy <ilya.yavorskiy.gj@gmail.com>
2021-01-06 13:13:04 +0200

    lesson6

--- lesson6_homework/num2_L6.py
+++ lesson6_homework/num2_L6.py
@@ -1,15 +1,17 @@
 class Road:
-    _length = None
-    _width = None
+    # _length = None
+    # _width = None
     weight = 25
     thickness = 5
 
     def __init__(self, length, width):
-        self.le = length
-        self.wi = width
+        # self.le = length
+        # self.wi = width
+        self._le = length
+        self._wi = width
 
     def my_func(self):
-        return (self.le * self.wi * self.weight * self.thickness) / 1000
+        return (self._le * self._wi * self.weight * self.thickness) / 1000
 
 
 r1 = Road(20, 5000)
