18b9dff3d1caa5aec70ece0f5f15cc3d613bb3b0
IlyaYavorskiy <ilya.yavorskiy.gj@gmail.com>
2021-02-27 14:14:30 +0200

    lesson8

--- lesson8_homework/num1_L8.py
+++ lesson8_homework/num1_L8.py
@@ -1,1 +1,31 @@
-pass
+class Data:
+    @classmethod
+    def num_transform(cls):
+        data_str = input('Введите число в формате день-месяц-год:')
+        numbers = cls.validation(data_str)
+        try:
+            print(f'День: {int(numbers[0])} Месяц: {int(numbers[1])} Год: {int(numbers[2])}')
+        except TypeError:
+            print('Программа выполнена некорректно')
+
+    @staticmethod
+    def validation(d_str):
+        try:
+            date_list = d_str.split('-')
+            if not 1 <= int(date_list[0]) <= 31:
+                print('Некорректный формат ввода дня')
+                return None
+            elif not 1 <= int(date_list[1]) <= 12:
+                print('Некорректный формат ввода месяца')
+                return None
+            elif not 1900 <= int(date_list[2]) <= 2020:
+                print('Некорректный формат ввода года')
+                return None
+        except ValueError:
+            return None
+        else:
+            return date_list
+
+
+num1 = Data()
+num1.num_transform()
